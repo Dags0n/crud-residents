b8ce69d311a593481d1618a625e124fb7f139115
Dagson Gabriel <dagson.g.01@gmail.com>
2024-03-19 01:48:21 -0300

    definindo rotas para as funções create, update e delete

--- back-end/routes/residents.js
+++ back-end/routes/residents.js
@@ -1,8 +1,14 @@
 import express from "express";
-import { getResidents } from "../controllers/resident.js";
+import { getResidents, createResident, updateResident, deleteResident } from "../controllers/resident.js";
 
 const router = express.Router();
 
 router.get("/", getResidents);
+
+router.post("/", createResident);
+
+router.put("/:cpf", updateResident);
+
+router.delete("/:cpf", deleteResident);
 
 export default router;
