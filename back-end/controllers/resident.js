57e6c5d0f4ce57fc9f4c2f516f85b3dcc8baa9fe
Dagson Gabriel <dagson.g.01@gmail.com>
2024-03-19 01:36:05 -0300

    criação da função para criar morador via POST

--- back-end/controllers/resident.js
+++ back-end/controllers/resident.js
@@ -1,14 +1,37 @@
 import { db } from "../db-connection.js";
 
 export const getResidents = (_, res) => {
-  const sql = "SELECT * FROM residents";
+  const query = "SELECT * FROM residents";
 
-  db.query(sql, (err, result) => {
+  db.query(query, (err, result) => {
     if (err) {
       res.status(500).json({ message: "Something went wrong" });
       return;
     }
 
     res.status(200).json(result);
+  });
+};
+
+export const createResident = (req, res) => {
+  const query = "INSERT INTO residents(`cpf`, `rg`, `nome`, `email`, phone, `date_birth`, `sex`) VALUES (?)";
+
+  const values = [
+    req.body.cpf,
+    req.body.rg,
+    req.body.nome,
+    req.body.email,
+    req.body.phone,
+    req.body.date_birth,
+    req.body.sex,
+  ];
+
+  db.query(query, [values], (err, result) => {
+    if (err) {
+      res.status(500).json({ message: "Something went wrong" });
+      return;
+    }
+
+    res.status(201).json({ message: "Resident created" });
   });
 };
